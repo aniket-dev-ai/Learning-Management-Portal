0f724a48975d24f1b38d876ec32ad7b9e8532779
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-03-07 16:21:39 +0530

    vite tailwind shadcn installed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <Button className="bg-blue-500 text-white">Hello Shadcn! 😍</Button>
+    </h1>
+  );
+}
