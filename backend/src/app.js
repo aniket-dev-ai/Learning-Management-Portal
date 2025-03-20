42b3ed5ded547af05958ac7efb73c993df5568ba
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-03-20 22:47:06 +0530

    updated cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5175", credentials: true }));
+app.use(cors({ origin: "hhttps://learning-management-portal-delta.vercel.app/", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
